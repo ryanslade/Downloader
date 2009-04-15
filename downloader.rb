a611d412250a0c5a8175511b4deb4ebec6ba72f7
Ryan Slade <ryanslade@gmail.com>
2009-04-15 23:14:15 +0100

    Added check in case file has already been downloaded

--- downloader.rb
+++ downloader.rb
@@ -8,19 +8,24 @@ done = false
 
 while !done
 
-  puts "Downloading..."
+  puts "Downloading feed..."
   rss = SimpleRSS.parse open("http://tvrss.net/feed/eztv/")
 
   rss.items.select { |i| /apprentice/ =~ i.title.downcase }.each do |i|
-    puts "Item found! Downloading..."
     
-    open(i.title+".torrent", "wb") do |file|
-      file.write(Net::HTTP.get(URI.parse(i.link)))
+    filename = "#{i.title}.torrent"
+
+    unless File.exist?(filename)
+      puts "Item found! Downloading..."
+
+      open(filename, "wb") do |file|
+        file.write(Net::HTTP.get(URI.parse(i.link)))
+      end
     end
-    
+
     done = true
   end
-  
+
   puts "Item not found, sleeping for #{MINUTES} minutes"
   sleep 60*MINUTES
 
